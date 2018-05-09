602b5e5294a84dd0bfebd5c3b597b7782ef33ace
Marleen <marleenberg1990@gmail.com>
2018-05-09 21:33:57 +0200

    added test

---
+++ app/tests/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marleen
+ * Date: 09/05/2018
+ * Time: 21:31
+ */
+
+use App\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testFillable()
+    {
+        $user = new User(['name' => 'username', 'email' => 'mail@mail.com', 'password' => 'supersecret']);
+        $this->assertEquals('Karin Slaughter', $user->name);
+        $this->assertEquals('mail@mail.com', $user->email);
+        $this->assertEquals('supersecret', $user->password);
+    }
+}
