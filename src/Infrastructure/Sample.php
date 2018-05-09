168990fd4a67990e0846660b1a0603d3a9a57f13
Marleen <marleenberg1990@gmail.com>
2018-05-09 21:52:11 +0200

    testing

---
+++ src/Infrastructure/Sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marleen
+ * Date: 09/05/2018
+ * Time: 21:47
+ */
+
+namespace src\Infrastructure;
+
+
+class Sample
+{
+    /**
+     * @return string
+     */
+    public function hello(): string
+    {
+        return 'hello';
+    }
+
+}
---
+++ src/Infrastructure/Tests/SampleTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marleen
+ * Date: 09/05/2018
+ * Time: 21:49
+ */
+
+namespace src\Infrastructure\Tests;
+
+use src\Infrastructure\Sample;
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * @var Sample
+     */
+    private $sample;
+
+    protected function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        $this->sample = new Sample();
+    }
+
+    public function testHello()
+    {
+        $this->assertEquals('hello', $this->sample->hello());
+    }
+
+}
