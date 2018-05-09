fac92738563c219066e87ffa2b451f657efcc904
Marleen <marleenberg1990@gmail.com>
2018-05-09 21:35:40 +0200

    fixed test

--- app/tests/UserTest.php
+++ app/tests/UserTest.php
@@ -14,7 +14,7 @@ class UserTest extends TestCase
     public function testFillable()
     {
         $user = new User(['name' => 'username', 'email' => 'mail@mail.com', 'password' => 'supersecret']);
-        $this->assertEquals('Karin Slaughter', $user->name);
+        $this->assertEquals('username', $user->name);
         $this->assertEquals('mail@mail.com', $user->email);
         $this->assertEquals('supersecret', $user->password);
     }
